377405b0cf314030257d8842561a82e9986afdbf
omaralim.eth <50689130+omaralim111@users.noreply.github.com>
2023-11-28 20:33:23 -0500

    From shodan to import shodan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os 
-from shodan
+import shodan
 from adb_shell.adb_device import AdbDeviceTcp
 from adb_shell.auth.sign_pythonrsa import PythonRSASigner
 from adb_shell.auth.keygen import keygen
